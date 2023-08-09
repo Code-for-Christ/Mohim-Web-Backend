5daf32f195b37a554a995a65f21875adca56e305
kimsungkyum <sungkyum1@naver.com>
2023-08-09 11:58:55 +0900

    Spring Admin Project Upload

---
+++ src/main/java/com/mohim/api/MohimApplication.java
@@ -0,0 +1,13 @@
+package com.mohim.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohim/api/MohimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohim.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
