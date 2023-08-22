7020c31a9de04118f945275c6f240e9655ae15c5
SungKyum Kim <sungkyum1@naver.com>
2023-08-22 13:12:44 +0900

    Feat: add Role Domain

--- src/main/java/com/mohim/api/domain/Role.java
+++ src/main/java/com/mohim/api/domain/Role.java
@@ -1,2 +1,21 @@
-package com.mohim.api.domain;public class Role {
+package com.mohim.api.domain;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private Auth auth;
 }
