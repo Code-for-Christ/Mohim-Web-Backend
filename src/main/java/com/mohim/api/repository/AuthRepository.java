704112e2dbabcacf69c3f19f9e659ce59529811a
SungKyum Kim <sungkyum1@naver.com>
2023-08-22 16:48:55 +0900

    Feat: add AuthRepository

---
+++ src/main/java/com/mohim/api/repository/AuthRepository.java
@@ -0,0 +1,11 @@
+package com.mohim.api.repository;
+
+import com.mohim.api.domain.Auth;
+import com.mohim.api.domain.ChurchMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AuthRepository extends JpaRepository<Auth, Long> {
+    public Optional<Auth> findByEmail(String email);
+}
