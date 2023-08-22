7895196cbaddfc74c6a42f85569830c5b18cf4ae
SungKyum Kim <sungkyum1@naver.com>
2023-08-22 16:58:42 +0900

    Feat: AuthService 추가

--- src/main/java/com/mohim/api/service/AuthService.java
+++ src/main/java/com/mohim/api/service/AuthService.java
@@ -1,2 +1,24 @@
-package com.mohim.api.service;public class AuthService {
+package com.mohim.api.service;
+
+import com.mohim.api.domain.Auth;
+import com.mohim.api.repository.AuthRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthRepository authRepository;
+
+    public Auth getUser(Long id) {
+        return authRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found user with id =" + id));
+    }
+
+    public Optional<Auth> getUser(String email) {
+        return authRepository.findByEmail(email);
+    }
+
 }
