9a88febbb8f5067684837eabc6100685e74def9e
SungKyum Kim <sungkyum1@naver.com>
2023-08-23 16:13:19 +0900

    Test: CustomUserDetailsServiceTest 추가 및 이메일로 권환 조회 테스트 검증

--- src/test/java/com/mohim/api/service/CustomUserDetailsServiceTest.java
+++ src/test/java/com/mohim/api/service/CustomUserDetailsServiceTest.java
@@ -1,4 +1,34 @@
+package com.mohim.api.service;
+
+import com.mohim.api.repository.AuthRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Rollback(value = true)
 class CustomUserDetailsServiceTest {
-  
+
+    @Autowired
+    AuthRepository authRepository;
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
+    @Test
+    @DisplayName("유저 이메일로 권환 조회")
+    void loadUserByUsername() {
+        // given
+        UserDetails user = customUserDetailsService.loadUserByUsername("sungkyum1@gmail.com");
+        // then
+        assertThat(user.getAuthorities()).extracting(GrantedAuthority::getAuthority)
+                .contains("ROLE_ADMIN", "WRITE_AUTHORITY");
+        System.out.println("user's Authorities : " + user.getAuthorities());
+    }
 }
