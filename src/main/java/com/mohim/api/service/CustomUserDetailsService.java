a3d17e0cb52e67571b0371428330cf3e4407d4fa
SungKyum Kim <sungkyum1@naver.com>
2023-08-23 16:11:50 +0900

    Feat: 유저의 역할과 권환을 조회하여 UserDetail에 설정해주는 서비스계층 추가

--- src/main/java/com/mohim/api/service/CustomUserDetailsService.java
+++ src/main/java/com/mohim/api/service/CustomUserDetailsService.java
@@ -1,2 +1,53 @@
-package com.mohim.api.service;public class CustomUserDetailsService {
+package com.mohim.api.service;
+
+import com.mohim.api.domain.Auth;
+import com.mohim.api.domain.Role;
+import com.mohim.api.domain.RolePrevilegeAssociation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    final private AuthService authService;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Auth auth = authService.getUser(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User is not found. email=" + email));
+
+        auth.setAuthorities(
+                Stream.concat(
+                        getRoles(auth.getRoles()).stream(),
+                        getPrivileges(auth.getRoles()).stream()
+                ).collect(Collectors.toList())
+        );
+
+        return auth;
+    }
+
+    private List<SimpleGrantedAuthority> getRoles(List<Role> roles) {
+        return roles.stream()
+                .map(Role::getName)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    private List<SimpleGrantedAuthority> getPrivileges(List<Role> roles) {
+        return roles.stream()
+                .flatMap(role -> role.getRolePrevilegeAssociations().stream())
+                .map(RolePrevilegeAssociation::getPrevilege)
+                .map(privilege -> new SimpleGrantedAuthority(privilege.getName()))
+                .collect(Collectors.toList());
+    }
 }
