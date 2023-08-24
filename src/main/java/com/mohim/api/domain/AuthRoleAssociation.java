28e59445976bbf1fc8f5127dcde6315005ff8604
SungKyum Kim <sungkyum1@naver.com>
2023-08-24 11:44:49 +0900

    Feat: AuthRoleAssociation Entity 추가

--- src/main/java/com/mohim/api/domain/AuthRoleAssociation.java
+++ src/main/java/com/mohim/api/domain/AuthRoleAssociation.java
@@ -1,2 +1,26 @@
-package com.mohim.api.domain;public class AuthRoleAssociation {
+package com.mohim.api.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AuthRoleAssociation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "auth_id")
+    private Auth auth;
+
 }
