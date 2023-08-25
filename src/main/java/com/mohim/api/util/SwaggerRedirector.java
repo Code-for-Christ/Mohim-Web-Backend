effcafda72fb8f7cde1c1ff46eeda5b8017048b6
SungKyum Kim <sungkyum1@naver.com>
2023-08-25 18:08:16 +0900

    Chore: swagger url redirect 설정

--- src/main/java/com/mohim/api/util/SwaggerRedirector.java
+++ src/main/java/com/mohim/api/util/SwaggerRedirector.java
@@ -1,2 +1,12 @@
-package com.mohim.api.util;public class SwaggerRedirector {
+package com.mohim.api.util;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/docs")
+class SwaggerRedirector {
+    @GetMapping
+    public String api() { return "redirect:/swagger-ui/index.html"; }
 }
