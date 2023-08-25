eb539f8a8e90580cd553fcc5d00996d648038231
SungKyum Kim <sungkyum1@naver.com>
2023-08-25 18:06:01 +0900

    Feat: 회원가입 API DTO 추가

--- src/main/java/com/mohim/api/dto/AuthJoinRequest.java
+++ src/main/java/com/mohim/api/dto/AuthJoinRequest.java
@@ -1,2 +1,18 @@
-package com.mohim.api.dto;public class AuthJoinRequest {
+package com.mohim.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.validation.constraints.NotBlank;
+
+@NoArgsConstructor
+@Getter
+public class AuthJoinRequest {
+
+    @NotBlank(message = "이메일을 입력하세요")
+    private String email;
+    @NotBlank(message = "비밀번호를 입력하세요")
+    private String password;
 }
