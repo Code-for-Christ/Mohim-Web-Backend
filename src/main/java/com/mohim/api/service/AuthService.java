10c1d8c9ea213f5e01ccdb47a6872bbf12a19f13
SungKyum Kim <sungkyum1@naver.com>
2023-08-25 18:06:44 +0900

    Feat: 회원가입 API를 위한 서비스 계층 구현

--- src/main/java/com/mohim/api/service/AuthService.java
+++ src/main/java/com/mohim/api/service/AuthService.java
@@ -1,8 +1,10 @@
 package com.mohim.api.service;
 
 import com.mohim.api.domain.Auth;
+import com.mohim.api.dto.*;
 import com.mohim.api.repository.AuthRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,6 +14,14 @@ import java.util.Optional;
 public class AuthService {
 
     private final AuthRepository authRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    public void join(AuthJoinRequest authJoinRequest) {
+        Auth auth = Auth.createAuth(authJoinRequest.getEmail(), bCryptPasswordEncoder.encode(authJoinRequest.getPassword()));
+        authRepository.save(auth);
+    }
+
 
     public Auth getUser(Long id) {
         return authRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found user with id =" + id));
