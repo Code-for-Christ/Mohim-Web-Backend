fdaf47c1cdf78ecb6a7f9f7277687b902046a504
SungKyum Kim <sungkyum1@naver.com>
2023-08-25 18:05:13 +0900

    Feat: 회원가입 API 구현

--- src/main/java/com/mohim/api/controller/AuthController.java
+++ src/main/java/com/mohim/api/controller/AuthController.java
@@ -1,4 +1,26 @@
 package com.mohim.api.controller;
 
+import com.mohim.api.dto.AuthJoinRequest;
+import com.mohim.api.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@Slf4j
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/join")
+    public ResponseEntity<Void> join(@RequestBody AuthJoinRequest authJoinRequest) {
+
+        authService.join(authJoinRequest);
+        return ResponseEntity.ok().build();
+    }
 }
