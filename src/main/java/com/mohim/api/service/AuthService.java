c78769d0f41b7358651156f1b2f4e6a1eea7c1f0
SungKyum Kim <sungkyum1@naver.com>
2023-08-27 01:16:00 +0900

    Feat: 로그인 API 추가

--- src/main/java/com/mohim/api/service/AuthService.java
+++ src/main/java/com/mohim/api/service/AuthService.java
@@ -1,24 +1,35 @@
 package com.mohim.api.service;
 
 import com.mohim.api.domain.Auth;
+import com.mohim.api.domain.AuthRoleAssociation;
+import com.mohim.api.domain.Role;
 import com.mohim.api.dto.*;
+import com.mohim.api.exception.CustomException;
+import com.mohim.api.exception.ErrorCode;
 import com.mohim.api.repository.AuthRepository;
+import com.mohim.api.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final AuthRepository authRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
 
     public void join(AuthJoinRequest authJoinRequest) {
-        Auth auth = Auth.createAuth(authJoinRequest.getEmail(), bCryptPasswordEncoder.encode(authJoinRequest.getPassword()));
+        Auth auth = Auth.createAuth(authJoinRequest.getEmail(), passwordEncoder.encode(authJoinRequest.getPassword()));
         authRepository.save(auth);
     }
 
@@ -31,4 +42,29 @@ public class AuthService {
         return authRepository.findByEmail(email);
     }
 
+    public AuthLoginResponse login(AuthLoginRequest request) {
+        Optional<Auth> auth = authRepository.findByEmail(request.getEmail());
+
+        if(auth.isEmpty()) {
+            throw new CustomException(ErrorCode.UNAUTHORIZED_ID);
+        }
+        if(!passwordEncoder.matches(request.getPassword(), auth.get().getPassword())) {
+            throw new CustomException(ErrorCode.UNAUTHORIZED_PASSWORD);
+        }
+
+        // 토큰 생성
+        String accessToken = jwtTokenProvider.generateAccessToken(auth.get());
+
+        AuthLoginResponse response = AuthLoginResponse.builder()
+                .accessToken(accessToken)
+                .email(auth.get().getEmail())
+                .role(auth.get().getAuthRoleAssociations().stream()
+                        .map(AuthRoleAssociation::getRole)
+                        .map(Role::getName)
+                        .collect(Collectors.toList()))
+                .build();
+
+        return response;
+
+    }
 }
