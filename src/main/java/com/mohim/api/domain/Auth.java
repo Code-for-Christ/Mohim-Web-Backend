f0b661162e7020495c1097f6f84d80b751d89457
SungKyum Kim <sungkyum1@naver.com>
2023-09-01 02:24:14 +0900

    Feat: Church 관계 설정:

--- src/main/java/com/mohim/api/domain/Auth.java
+++ src/main/java/com/mohim/api/domain/Auth.java
@@ -1,9 +1,6 @@
 package com.mohim.api.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
@@ -17,6 +14,7 @@ import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Auth implements UserDetails {
 
