ba47e802f0fcebc1179287e7dd7e903ab7fc8c3c
SungKyum Kim <sungkyum1@naver.com>
2023-09-01 02:29:03 +0900

    Feat: 인증 요청 API 리퀘스트 DTO 구현

--- src/main/java/com/mohim/api/dto/AuthAuthenticateRequest.java
+++ src/main/java/com/mohim/api/dto/AuthAuthenticateRequest.java
@@ -1,2 +1,15 @@
-package com.mohim.api.dto;public class AuthAuthenticateRequest {
+package com.mohim.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AuthAuthenticateRequest {
+    private String name;
+    private String phoneNumber;
+    private Long churchId;
 }
