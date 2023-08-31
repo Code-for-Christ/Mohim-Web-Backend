890de9f3ee50be8dd382b9e24094c96560a103aa
SungKyum Kim <sungkyum1@naver.com>
2023-09-01 02:30:16 +0900

    Feat: MemberRepository 구현

--- src/main/java/com/mohim/api/repository/MemberRepository.java
+++ src/main/java/com/mohim/api/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.mohim.api.repository;
 import com.mohim.api.domain.ChurchMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<ChurchMember, Long> {
+
+    public Optional<ChurchMember> findByNameAndPhoneNumber(String name, String phoneNumber);
 }
