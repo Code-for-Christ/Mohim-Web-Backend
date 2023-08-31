c7d23f74210d7d9295585567ac4f6c367d4564b8
SungKyum Kim <sungkyum1@naver.com>
2023-09-01 02:29:57 +0900

    Feat: ChurchRepository 구현

--- src/main/java/com/mohim/api/repository/ChurchRepository.java
+++ src/main/java/com/mohim/api/repository/ChurchRepository.java
@@ -1,2 +1,7 @@
-package com.mohim.api.repository;public class ChurchRepository {
+package com.mohim.api.repository;
+
+import com.mohim.api.domain.Church;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChurchRepository extends JpaRepository<Church, Long> {
 }
