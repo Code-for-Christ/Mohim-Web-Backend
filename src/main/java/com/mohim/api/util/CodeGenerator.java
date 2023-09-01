29d3820ec1b82add68d939a2982cf16dd2a4222a
SungKyum Kim <sungkyum1@naver.com>
2023-09-01 17:54:26 +0900

    Feat: 임시코드 발송을 위한 코드 제너레이터 구현

--- src/main/java/com/mohim/api/util/CodeGenerator.java
+++ src/main/java/com/mohim/api/util/CodeGenerator.java
@@ -1,2 +1,14 @@
-package com.mohim.api.util;public class CodeGenerator {
+package com.mohim.api.util;
+
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class CodeGenerator {
+
+    public String generateEightCharacterCode() {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString().replaceAll("-", "").substring(0, 8);
+    }
 }
