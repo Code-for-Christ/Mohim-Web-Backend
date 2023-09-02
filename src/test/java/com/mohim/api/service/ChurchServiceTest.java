a1611471a7ed7616559a5e046a4844394de9c451
SungKyum Kim <sungkyum1@naver.com>
2023-09-03 00:53:48 +0900

    Test: ChurchService Test 추가

--- src/test/java/com/mohim/api/service/ChurchServiceTest.java
+++ src/test/java/com/mohim/api/service/ChurchServiceTest.java
@@ -1,4 +1,35 @@
+package com.mohim.api.service;
+
+import com.mohim.api.dto.ChurchResponse;
+import com.mohim.api.repository.ChurchRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class ChurchServiceTest {
-  
+
+    @Autowired
+    ChurchRepository churchRepository;
+
+    @Autowired
+    ChurchService churchService;
+
+
+    @Test
+    @DisplayName("교회 리스트 가져오기")
+    void getChurchList() {
+        List<ChurchResponse> churches = churchService.getChurchList();
+
+        assertThat(churches.size()).isEqualTo(1);
+
+        System.out.println("churches.get(0).getName() = " + churches.get(0).getName());
+    }
 }
