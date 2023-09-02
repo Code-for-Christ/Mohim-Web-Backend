bbf4d89253c9430f2c727efcaa416c25d48757f5
SungKyum Kim <sungkyum1@naver.com>
2023-09-03 00:53:23 +0900

    Feat: Get Church List API DTO 구현

--- src/main/java/com/mohim/api/dto/ChurchResponse.java
+++ src/main/java/com/mohim/api/dto/ChurchResponse.java
@@ -1,2 +1,12 @@
-package com.mohim.api.dto;public class ChurchResponse {
+package com.mohim.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ChurchResponse {
+    private Long id;
+    private String name;
+    private String country;
 }
