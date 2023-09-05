65d2adb2b58b3b9748f9e8340d655c97e52773b2
SungKyum Kim <sungkyum1@naver.com>
2023-09-06 01:08:42 +0900

    Feat: GetChurchMembers API 구현을 위한 DTO 추가

--- src/main/java/com/mohim/api/dto/ChurchMembersRequest.java
+++ src/main/java/com/mohim/api/dto/ChurchMembersRequest.java
@@ -1,2 +1,23 @@
-package com.mohim.api.dto;public class ChurchMembersRequest {
+package com.mohim.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class ChurchMembersRequest {
+    Integer memberId;
+    Integer householderId;
+    Integer parish;
+    Integer cellId;
+    Integer gatheringId;
+    Integer positionId;
+//    OrderBy orderBy = OrderBy.NAME;
+    List<String> orderBy = new ArrayList<>(List.of("name"));
+    Integer page = 1;
+    Integer size = 20;
+    String search;
 }
