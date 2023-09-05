07aa735f80db946923386ade0d66f42a9c1c9b3b
SungKyum Kim <sungkyum1@naver.com>
2023-09-06 01:08:06 +0900

    Feat: GetChurchMembers API 구현

--- src/main/java/com/mohim/api/service/ChurchService.java
+++ src/main/java/com/mohim/api/service/ChurchService.java
@@ -1,12 +1,16 @@
 package com.mohim.api.service;
 
 import com.mohim.api.domain.Church;
+import com.mohim.api.dto.ChurchMembersRequest;
+import com.mohim.api.dto.ChurchMembersResponse;
 import com.mohim.api.dto.ChurchResponse;
 import com.mohim.api.repository.ChurchRepository;
+import com.mohim.api.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,6 +20,7 @@ import java.util.stream.Collectors;
 public class ChurchService {
 
     private final ChurchRepository churchRepository;
+    private final MemberRepository memberRepository;
 
     public List<ChurchResponse> getChurchList() {
 
@@ -28,5 +33,61 @@ public class ChurchService {
                         .country(church.getCountry())
                         .build())
                 .collect(Collectors.toList());
+    }
+
+    public ChurchMembersResponse getChurchMembers(Integer churchId, ChurchMembersRequest request, HttpServletRequest httpServletRequest) {
+        List<ChurchMembersResponse.ChurchMember> churchMembers = memberRepository.findByChurchId(churchId, request).stream()
+                .map(churchMember -> ChurchMembersResponse.ChurchMember.builder()
+                        .id(churchMember.getId())
+                        .name(churchMember.getName())
+                        .householderId(churchMember.getHouseholderId())
+                        .relationshipWithHouseholder(churchMember.getRelationshipWithHouseHolder())
+                        .birthYear(churchMember.getBirthYear())
+                        .salvationYear(churchMember.getSalvationYear())
+                        .salvationMonth(churchMember.getSalvationMonth())
+                        .salvationDay(churchMember.getSalvationDay())
+                        .carNumber(churchMember.getCarNumber())
+                        .sex(churchMember.getGender().getValue())
+                        .phoneNumber(churchMember.getPhoneNumber())
+                        .address(churchMember.getAddress())
+                        .profileImageThumbnail(churchMember.getProfileImageThumbnail())
+                        .churchId(churchMember.getChurch().getId())
+                        .churchName(churchMember.getChurch().getName())
+                        .cell(churchMember.getCell().getCell())
+                        .cellId(churchMember.getCell().getId())
+                        .gatheringId(churchMember.getGathering().getId())
+                        .gatheringName(churchMember.getGathering().getName())
+                        .positionId(churchMember.getPosition() == null? null : churchMember.getPosition().getId())
+                        .positionName(churchMember.getPosition() == null? null : churchMember.getPosition().getName())
+                        .build())
+                .collect(Collectors.toList());
+
+        Integer totalCount = memberRepository.getTotalCount(churchId, request);
+
+        Integer page = request.getPage();
+        Integer size = request.getSize();
+        String nextUrl = null;
+        if (page * size < totalCount) {
+            nextUrl = httpServletRequest.getRequestURL().toString();
+            nextUrl += "?" + httpServletRequest.getQueryString();
+
+            String prePage = String.format("page=%d", page);
+            String nextPage = String.format("page=%d", page+1);
+            nextUrl = nextUrl.replace(prePage, nextPage);
+        }
+
+
+        ChurchMembersResponse.Metadata metadata = ChurchMembersResponse.Metadata.builder()
+                .totalCount(totalCount)
+                .page(request.getPage())
+                .size(request.getSize())
+                .orderBy(request.getOrderBy())
+                .nextUrl(nextUrl)
+                .build();
+
+        return ChurchMembersResponse.builder()
+                .metadata(metadata)
+                .churchMembers(churchMembers)
+                .build();
     }
 }
