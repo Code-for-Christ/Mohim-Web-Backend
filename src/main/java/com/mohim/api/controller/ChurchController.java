1b9db1f402ecec6cb88e711ca1543f2ec2f0dad8
SungKyum Kim <sungkyum1@naver.com>
2023-09-06 01:08:06 +0900

    Feat: GetChurchMembers API 구현

--- src/main/java/com/mohim/api/controller/ChurchController.java
+++ src/main/java/com/mohim/api/controller/ChurchController.java
@@ -1,14 +1,15 @@
 package com.mohim.api.controller;
 
+import com.mohim.api.dto.ChurchMembersRequest;
+import com.mohim.api.dto.ChurchMembersResponse;
 import com.mohim.api.dto.ChurchResponse;
 import com.mohim.api.service.ChurchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -24,5 +25,13 @@ public class ChurchController {
     public ResponseEntity<List<ChurchResponse>> getChurchList() {
         List<ChurchResponse> responses = churchService.getChurchList();
         return ResponseEntity.ok().body(responses);
+    }
+
+    @GetMapping("/{church_id}/members")
+    public ResponseEntity<ChurchMembersResponse> getChurchMembers(@PathVariable("church_id") Integer churchId, @ModelAttribute ChurchMembersRequest request, HttpServletRequest httpServletRequest) {
+        System.out.println("request = " + request.getSize());
+        System.out.println("request.getCellId() = " + request.getCellId());
+        ChurchMembersResponse response = churchService.getChurchMembers(churchId, request, httpServletRequest);
+        return ResponseEntity.ok().body(response);
     }
 }
