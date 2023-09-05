57afafe0aba105d0d0fbc1bb4fb9540410bc6b6b
SungKyum Kim <sungkyum1@naver.com>
2023-09-06 01:05:58 +0900

    Feat: Position Domain 구현

--- src/main/java/com/mohim/api/domain/Position.java
+++ src/main/java/com/mohim/api/domain/Position.java
@@ -1,2 +1,21 @@
-package com.mohim.api.domain;public class Position {
+package com.mohim.api.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Position {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "church_id")
+    private Church church;
 }
