18cd48bebdbbd29aed4f948b81294db8c20a55c5
SungKyum Kim <sungkyum1@naver.com>
2023-09-06 01:09:29 +0900

    Feat: Query DSL 설정 및 동적 쿼리 구현

--- src/main/java/com/mohim/api/repository/MemberRepositoryCustom.java
+++ src/main/java/com/mohim/api/repository/MemberRepositoryCustom.java
@@ -1,2 +1,11 @@
-package com.mohim.api.repository;public class MemberRepositoryCustom {
+package com.mohim.api.repository;
+
+import com.mohim.api.domain.ChurchMember;
+import com.mohim.api.dto.ChurchMembersRequest;
+
+import java.util.List;
+
+public interface MemberRepositoryCustom {
+    List<ChurchMember> findByChurchId(Integer churchId, ChurchMembersRequest request);
+    Integer getTotalCount(Integer churchId, ChurchMembersRequest request);
 }
