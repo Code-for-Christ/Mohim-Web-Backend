8b4e3474c28728b2cb48d5a6952d0904144b5bd9
SungKyum Kim <sungkyum1@naver.com>
2023-09-05 15:07:09 +0900

    Feat: Parish Domain 구현

--- src/main/java/com/mohim/api/domain/Parish.java
+++ src/main/java/com/mohim/api/domain/Parish.java
@@ -1,2 +1,14 @@
-package com.mohim.api.domain;public class Parish {
+package com.mohim.api.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Parish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "church_id")
+    private Church church;
 }
