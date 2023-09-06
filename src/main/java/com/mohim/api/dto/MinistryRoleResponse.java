3bfa7779b7110b3ca0ee810a40399bfa4913c17b
SungKyum Kim <sungkyum1@naver.com>
2023-09-07 00:34:08 +0900

    Feat: ministry Role response DTO 구현

--- src/main/java/com/mohim/api/dto/MinistryRoleResponse.java
+++ src/main/java/com/mohim/api/dto/MinistryRoleResponse.java
@@ -1,2 +1,13 @@
-package com.mohim.api.dto;public class MinistryRoleResponse {
+package com.mohim.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class MinistryRoleResponse {
+    private Integer ministryId;
+    private String ministryName;
+    private String ministryRole;
+
 }
