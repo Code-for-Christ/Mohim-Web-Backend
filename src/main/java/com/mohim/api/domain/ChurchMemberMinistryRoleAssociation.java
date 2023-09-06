420e3a96e89e70bcc944811ce928f6e338e88bf5
SungKyum Kim <sungkyum1@naver.com>
2023-09-07 00:33:00 +0900

    Feat: Ministry 관련 도메인 추가 및 연관 관계 설정

--- src/main/java/com/mohim/api/domain/ChurchMemberMinistryRoleAssociation.java
+++ src/main/java/com/mohim/api/domain/ChurchMemberMinistryRoleAssociation.java
@@ -1,2 +1,24 @@
-package com.mohim.api.domain;public class ChurchMemberMinistryRoleAssociation {
+package com.mohim.api.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChurchMemberMinistryRoleAssociation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "church_member_id")
+    private ChurchMember churchMember;
+
+    @ManyToOne
+    @JoinColumn(name = "ministry_role_id")
+    private MinistryRole ministryRole;
 }
