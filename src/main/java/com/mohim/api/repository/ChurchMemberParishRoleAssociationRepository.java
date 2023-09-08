dcaa7ff08c585118ed735da7b0af71e61519fdaf
SungKyum Kim <sungkyum1@naver.com>
2023-09-08 19:00:22 +0900

    Feat: 필요 레포지토리 구현

--- src/main/java/com/mohim/api/repository/ChurchMemberParishRoleAssociationRepository.java
+++ src/main/java/com/mohim/api/repository/ChurchMemberParishRoleAssociationRepository.java
@@ -1,4 +1,9 @@
 package com.mohim.api.repository;
 
-public class ChurchMemberParishRoleAssociation {
+import com.mohim.api.domain.ChurchMember;
+import com.mohim.api.domain.ChurchMemberParishRoleAssociation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChurchMemberParishRoleAssociationRepository extends JpaRepository<ChurchMemberParishRoleAssociation, Long> {
+    ChurchMemberParishRoleAssociation findByChurchMember(ChurchMember churchMember);
 }
