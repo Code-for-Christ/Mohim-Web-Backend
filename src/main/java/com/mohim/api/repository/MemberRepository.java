33b5ea0b627ec9b9143bb219d7e215a7ed49f5fd
SungKyum Kim <sungkyum1@naver.com>
2023-09-08 18:58:40 +0900

    Feat: GetGhurchMember API 구현

--- src/main/java/com/mohim/api/repository/MemberRepository.java
+++ src/main/java/com/mohim/api/repository/MemberRepository.java
@@ -3,10 +3,12 @@ package com.mohim.api.repository;
 import com.mohim.api.domain.ChurchMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<ChurchMember, Long>, MemberRepositoryCustom {
 
     Optional<ChurchMember> findByNameAndPhoneNumber(String name, String phoneNumber);
     Optional<ChurchMember> findByIdAndChurchId(Long id, Long churchId);
+    List<ChurchMember> findAllByHouseholderId(Long householderId);
 }
