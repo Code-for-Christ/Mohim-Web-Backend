a0ce0613cd9d7a312e7035e7dac6bae5b99f8e5b
SungKyum Kim <sungkyum1@naver.com>
2023-09-08 18:57:56 +0900

    Feat: GetGhurchMember API 를 위한 DTO 객체 구현

--- src/main/java/com/mohim/api/dto/ChurchMemberResponse.java
+++ src/main/java/com/mohim/api/dto/ChurchMemberResponse.java
@@ -1,2 +1,60 @@
-package com.mohim.api.dto;public class ChurchMemberResponse {
+package com.mohim.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+public class ChurchMemberResponse {
+    private Long id;
+    private String name;
+    private Long householderId;
+    private String relationshipWithHouseholder;
+    private Integer birthYear;
+    private Integer salvationYear;
+    private Integer salvationMonth;
+    private Integer salvationDay;
+    private String carNumber;
+    private String sex;
+    private String phoneNumber;
+    private String address;
+    private byte[] profileImageThumbnail;
+    private Long churchId;
+    private String churchName;
+    private Long cellId;
+    private Integer cell;
+    private Long gatheringId;
+    private String gatheringName;
+    private Long positionId;
+    private String positionName;
+    private Integer parish;
+    private String parishRole;
+    private String cellRole;
+    private String gatheringRole;
+    private List<Household> household;
+    private List<Ministry> ministries;
+
+    @Builder
+    @Getter
+    public static class Household {
+        private Long id;
+        private String name;
+        private Long householderId;
+        private String relationshipWithHouseholder;
+        private String phoneNumber;
+        private byte[] profileImageThumbnail;
+    }
+
+    @Builder
+    @Getter
+    @Setter
+    public static class Ministry {
+        private Long id;
+        private String name;
+        private String role;
+    }
 }
