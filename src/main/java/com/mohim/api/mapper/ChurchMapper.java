c2f8feac2af8a00a0fc3e437df7da9e2d5a7edbd
SungKyum Kim <sungkyum1@naver.com>
2023-09-10 01:18:45 +0900

    Feat: Church Mapper Class 적용

--- src/main/java/com/mohim/api/mapper/ChurchMapper.java
+++ src/main/java/com/mohim/api/mapper/ChurchMapper.java
@@ -1,2 +1,25 @@
-package com.mohim.api.mapper;public class ChurchMapper {
+package com.mohim.api.mapper;
+
+import com.mohim.api.domain.Church;
+import com.mohim.api.dto.ChurchDTO;
+import com.mohim.api.dto.ChurchesResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ChurchMapper {
+    public ChurchesResponse toChurchesResponse(List<ChurchDTO> churchDTOS) {
+        return ChurchesResponse.builder()
+                .churches(churchDTOS)
+                .build();
+    }
+
+    public ChurchDTO toChurchDTO(Church church){
+        return ChurchDTO.builder()
+                .id(church.getId())
+                .name(church.getName())
+                .country(church.getCountry())
+                .build();
+    }
 }
