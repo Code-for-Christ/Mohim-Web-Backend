030cb6d2f786a9030032f114d45f791f0f85f821
SungKyum Kim <sungkyum1@naver.com>
2023-09-10 00:53:05 +0900

    Feat: GetCellList API 구현

--- src/main/java/com/mohim/api/service/CellService.java
+++ src/main/java/com/mohim/api/service/CellService.java
@@ -1,2 +1,27 @@
-package com.mohim.api.service;public class CellService {
+package com.mohim.api.service;
+
+import com.mohim.api.domain.Cell;
+import com.mohim.api.dto.CellResponse;
+import com.mohim.api.mapper.CellMapper;
+import com.mohim.api.repository.CellRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CellService {
+
+    private final CellRepository cellRepository;
+    private final CellMapper cellMapper;
+
+    public List<CellResponse> getCellList(Long churchId) {
+        List<Cell> cellList = cellRepository.findAllByChurchId(churchId);
+
+        return cellList.stream()
+                .map(cell -> cellMapper.toCellResponse(cell))
+                .collect(Collectors.toList());
+    }
 }
