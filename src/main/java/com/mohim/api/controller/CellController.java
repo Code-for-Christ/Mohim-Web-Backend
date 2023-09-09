3de5d2111be4665f998d6a64d085b56ba8388fd8
SungKyum Kim <sungkyum1@naver.com>
2023-09-10 00:53:05 +0900

    Feat: GetCellList API 구현

--- src/main/java/com/mohim/api/controller/CellController.java
+++ src/main/java/com/mohim/api/controller/CellController.java
@@ -1,2 +1,29 @@
-package com.mohim.api.controller;public class CellController {
+package com.mohim.api.controller;
+
+import com.mohim.api.dto.CellResponse;
+import com.mohim.api.service.CellService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/churches")
+@Slf4j
+@RequiredArgsConstructor
+public class CellController {
+
+    private final CellService cellService;
+
+    // 구역 목록 조회
+    @GetMapping("{church_id}/cells")
+    public ResponseEntity<List<CellResponse>> getCellList(@PathVariable(name = "church_id") Long churchId) {
+        List<CellResponse> response = cellService.getCellList(churchId);
+        return ResponseEntity.ok().body(response);
+    }
 }
