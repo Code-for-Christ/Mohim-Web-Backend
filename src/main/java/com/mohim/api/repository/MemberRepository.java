3ad1138a252e2a0074528ed660cb648fc75a9abc
SungKyum Kim <sungkyum1@naver.com>
2023-09-12 21:54:45 +0900

    Feat: get cell leaders API 구현

--- src/main/java/com/mohim/api/repository/MemberRepository.java
+++ src/main/java/com/mohim/api/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.mohim.api.repository;
 
 import com.mohim.api.domain.ChurchMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +13,11 @@ public interface MemberRepository extends JpaRepository<ChurchMember, Long>, Mem
     Optional<ChurchMember> findByNameAndPhoneNumber(String name, String phoneNumber);
     Optional<ChurchMember> findByIdAndChurchId(Long id, Long churchId);
     List<ChurchMember> findAllByHouseholderId(Long householderId);
+    @Query("SELECT cm FROM ChurchMember cm " +
+            "INNER JOIN cm.churchMemberCellRoleAssociations cra " +
+            "INNER JOIN cra.cellRole cr " +
+            "WHERE cm.church.id = :churchId " +
+            "AND cr.cell.id = :cellId " +
+            "ORDER BY cr.ordinalPosition ASC")
+    List<ChurchMember> findChurchMembersByCellIdAndChurchId(@Param("cellId") Long cellId, @Param("churchId") Long churchId);
 }
--- src/main/java/com/mohim/api/service/CellService.java
+++ src/main/java/com/mohim/api/service/CellService.java
@@ -1,10 +1,14 @@
 package com.mohim.api.service;
 
 import com.mohim.api.domain.Cell;
+import com.mohim.api.domain.ChurchMember;
 import com.mohim.api.dto.CellDTO;
+import com.mohim.api.dto.CellLeaderDTO;
+import com.mohim.api.dto.CellLeadersResponse;
 import com.mohim.api.dto.CellsResponse;
 import com.mohim.api.mapper.CellMapper;
 import com.mohim.api.repository.CellRepository;
+import com.mohim.api.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +21,7 @@ public class CellService {
 
     private final CellRepository cellRepository;
     private final CellMapper cellMapper;
+    private final MemberRepository memberRepository;
 
     public CellsResponse getCellList(Long churchId) {
         List<Cell> cellList = cellRepository.findAllByChurchId(churchId);
@@ -26,5 +31,16 @@ public class CellService {
                 .collect(Collectors.toList());
 
         return cellMapper.toCellsResponse(cellDTOS);
+    }
+
+    public CellLeadersResponse getCellLeaders(Long churchId, Long cellId) {
+        // 해당 셀 목록 가져오기
+        // 셀에 역할
+        List<CellLeaderDTO> cellLeaderDTOS = memberRepository.findChurchMembersByCellIdAndChurchId(cellId, churchId).stream()
+                .map(churchMember ->
+                    cellMapper.toCellLeaderDTO(churchMember)
+                ).collect(Collectors.toList());
+
+        return cellMapper.toCellLeadersResponse(cellLeaderDTOS);
     }
 }
