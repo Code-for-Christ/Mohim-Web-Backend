87d4cb835e27573d5e31353e2a4d56ea523fc703
SungKyum Kim <sungkyum1@naver.com>
2023-09-13 21:38:57 +0900

    Feat: Gatherings API 베이스 코드

--- src/main/java/com/mohim/api/dto/GatheringDTO.java
+++ src/main/java/com/mohim/api/dto/GatheringDTO.java
@@ -1,2 +1,12 @@
-package com.mohim.api.dto;public class GatheringDTO {
+package com.mohim.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class GatheringDTO {
+    private Long id;
+    private String name;
+    private Long churchId;
 }
--- src/main/java/com/mohim/api/service/GatheringService.java
+++ src/main/java/com/mohim/api/service/GatheringService.java
@@ -1,2 +1,14 @@
-package com.mohim.api.service;public class GatheringService {
+package com.mohim.api.service;
+
+import com.mohim.api.dto.GatheringsResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GatheringService {
+
+    public GatheringsResponse getGatheringList(Long churchId) {
+
+    }
 }
