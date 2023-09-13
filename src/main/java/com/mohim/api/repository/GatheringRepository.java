6a401b5da561c698a599cc064627bb0d7c2314cb
SungKyum Kim <sungkyum1@naver.com>
2023-09-14 00:39:09 +0900

    Fix: 봉사 목록 조회 API 수정

--- src/main/java/com/mohim/api/repository/GatheringRepository.java
+++ src/main/java/com/mohim/api/repository/GatheringRepository.java
@@ -3,5 +3,8 @@ package com.mohim.api.repository;
 import com.mohim.api.domain.Gathering;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GatheringRepository extends JpaRepository<Gathering, Long> {
+    List<Gathering> findByChurchId(Long church_id);
 }
