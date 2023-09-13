0211b8379222380594910c50c0515d915755c7be
SungKyum Kim <sungkyum1@naver.com>
2023-09-13 21:49:42 +0900

    Feat: get gathering list API 구현

--- src/main/java/com/mohim/api/service/GatheringService.java
+++ src/main/java/com/mohim/api/service/GatheringService.java
@@ -1,14 +1,30 @@
 package com.mohim.api.service;
 
+import com.mohim.api.domain.Gathering;
+import com.mohim.api.dto.GatheringDTO;
 import com.mohim.api.dto.GatheringsResponse;
+import com.mohim.api.mapper.GatheringMapper;
+import com.mohim.api.repository.GatheringRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class GatheringService {
 
-    public GatheringsResponse getGatheringList(Long churchId) {
+    private final GatheringRepository gatheringRepository;
+    private final GatheringMapper gatheringMapper;
 
+    public GatheringsResponse getGatheringList(Long churchId) {
+        List<GatheringDTO> gatheringDTOS = gatheringRepository.findAll().stream()
+                .map(gathering -> {
+                    return gatheringMapper.toGatheringDTO(gathering);
+                })
+                .collect(Collectors.toList());
+
+        return gatheringMapper.toGatheringsResponse(gatheringDTOS);
     }
 }
