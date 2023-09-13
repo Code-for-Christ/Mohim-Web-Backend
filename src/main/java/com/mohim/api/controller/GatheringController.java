ea690b2b870201af81e5ca63c227bb8e498d9509
SungKyum Kim <sungkyum1@naver.com>
2023-09-14 00:40:28 +0900

    Feat: 봉사 임원 목록 조회 API 구현

--- src/main/java/com/mohim/api/controller/GatheringController.java
+++ src/main/java/com/mohim/api/controller/GatheringController.java
@@ -1,6 +1,7 @@
 package com.mohim.api.controller;
 
 import com.mohim.api.dto.GatheringDTO;
+import com.mohim.api.dto.GatheringLeadersResponse;
 import com.mohim.api.dto.GatheringsResponse;
 import com.mohim.api.service.GatheringService;
 import lombok.RequiredArgsConstructor;
@@ -9,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/churches")
@@ -22,6 +21,12 @@ public class GatheringController {
     @GetMapping("/{church_id}/gatherings")
     public ResponseEntity<GatheringsResponse> getGatheringList(@PathVariable("church_id") Long churchId){
         GatheringsResponse response = gatheringService.getGatheringList(churchId);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/{church_id}/gatherings/{gathering_id}/leaders")
+    public ResponseEntity<GatheringLeadersResponse> getGatheringLeaders(@PathVariable("church_id") Long churchId, @PathVariable("gathering_id") Long gatheringId) {
+        GatheringLeadersResponse response = gatheringService.getGatheringLeaders(churchId, gatheringId);
         return ResponseEntity.ok().body(response);
     }
 }
--- src/main/java/com/mohim/api/service/GatheringService.java
+++ src/main/java/com/mohim/api/service/GatheringService.java
@@ -2,9 +2,12 @@ package com.mohim.api.service;
 
 import com.mohim.api.domain.Gathering;
 import com.mohim.api.dto.GatheringDTO;
+import com.mohim.api.dto.GatheringLeaderDTO;
+import com.mohim.api.dto.GatheringLeadersResponse;
 import com.mohim.api.dto.GatheringsResponse;
 import com.mohim.api.mapper.GatheringMapper;
 import com.mohim.api.repository.GatheringRepository;
+import com.mohim.api.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,15 +19,26 @@ import java.util.stream.Collectors;
 public class GatheringService {
 
     private final GatheringRepository gatheringRepository;
+    private final MemberRepository memberRepository;
     private final GatheringMapper gatheringMapper;
 
     public GatheringsResponse getGatheringList(Long churchId) {
-        List<GatheringDTO> gatheringDTOS = gatheringRepository.findAll().stream()
+        List<GatheringDTO> gatheringDTOS = gatheringRepository.findByChurchId(churchId).stream()
                 .map(gathering -> {
                     return gatheringMapper.toGatheringDTO(gathering);
                 })
                 .collect(Collectors.toList());
 
         return gatheringMapper.toGatheringsResponse(gatheringDTOS);
+    }
+
+    public GatheringLeadersResponse getGatheringLeaders(Long churchId, Long gatheringId) {
+        List<GatheringLeaderDTO> gatheringLeaderDTOS = memberRepository.findChurchMembersByGatheringIdAndChurchId(gatheringId, churchId).stream()
+                .map(churchMember -> {
+                    return gatheringMapper.toGatheringLeaderDTO(churchMember);
+                })
+                .collect(Collectors.toList());
+
+        return gatheringMapper.toGatheringLeadersResponse(gatheringLeaderDTOS);
     }
 }
