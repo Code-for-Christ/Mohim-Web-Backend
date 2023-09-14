fce7e5588b304b2e2a990f200ff79c4defcf1784
SungKyum Kim <sungkyum1@naver.com>
2023-09-14 23:39:15 +0900

    Feat: 회별 목록 조회 API 구현

--- src/main/java/com/mohim/api/service/MinistryService.java
+++ src/main/java/com/mohim/api/service/MinistryService.java
@@ -1,2 +1,28 @@
-package com.mohim.api.service;public class MinistryService {
+package com.mohim.api.service;
+
+import com.mohim.api.dto.MinistriesResponse;
+import com.mohim.api.dto.MinistryDTO;
+import com.mohim.api.mapper.MinistryMapper;
+import com.mohim.api.repository.MinistryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MinistryService {
+
+    private final MinistryRepository ministryRepository;
+    private final MinistryMapper ministryMapper;
+
+    public MinistriesResponse getMinistryList(Long churchId) {
+        List<MinistryDTO> ministryDTOS = ministryRepository.findByChurchId(churchId).stream()
+                .map(ministry -> {
+                    return ministryMapper.toMinistryDTO(ministry);
+                }).collect(Collectors.toList());
+        return ministryMapper.toMinistryResponse(ministryDTOS);
+    }
+
 }
