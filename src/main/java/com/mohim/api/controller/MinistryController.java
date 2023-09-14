a93761e5e7616ebff34225d3ed2b29e41255f285
SungKyum Kim <sungkyum1@naver.com>
2023-09-14 23:39:15 +0900

    Feat: 회별 목록 조회 API 구현

--- src/main/java/com/mohim/api/controller/MinistryController.java
+++ src/main/java/com/mohim/api/controller/MinistryController.java
@@ -1,2 +1,25 @@
-package com.mohim.api.controller;public class MinistryController {
+package com.mohim.api.controller;
+
+import com.mohim.api.dto.MinistriesResponse;
+import com.mohim.api.service.MinistryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/churches")
+@RequiredArgsConstructor
+public class MinistryController {
+
+    private final MinistryService ministryService;
+
+    @GetMapping("/{church_id}/ministries")
+    public ResponseEntity<MinistriesResponse> getMinistryList(@PathVariable("church_id") Long churchId) {
+        MinistriesResponse response = ministryService.getMinistryList(churchId);
+        return ResponseEntity.ok().body(response);
+    }
+
 }
