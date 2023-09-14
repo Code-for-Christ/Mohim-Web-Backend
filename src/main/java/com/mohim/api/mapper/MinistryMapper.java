744b82d2fecba744811ec977846d2eb5d25fae49
SungKyum Kim <sungkyum1@naver.com>
2023-09-15 00:23:48 +0900

    Feat: get Ministry Leaders API 구현

--- src/main/java/com/mohim/api/mapper/MinistryMapper.java
+++ src/main/java/com/mohim/api/mapper/MinistryMapper.java
@@ -1,8 +1,11 @@
 package com.mohim.api.mapper;
 
+import com.mohim.api.domain.ChurchMember;
 import com.mohim.api.domain.Ministry;
 import com.mohim.api.dto.MinistriesResponse;
 import com.mohim.api.dto.MinistryDTO;
+import com.mohim.api.dto.MinistryLeaderDTO;
+import com.mohim.api.dto.MinistryLeadersResponse;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -22,6 +25,41 @@ public class MinistryMapper {
     public MinistriesResponse toMinistryResponse(List<MinistryDTO> ministryDTOS) {
         return MinistriesResponse.builder()
                 .ministries(ministryDTOS)
+                .build();
+    }
+
+    public MinistryLeaderDTO toMinistryLeaderDTO(ChurchMember churchMember) {
+        return MinistryLeaderDTO.builder()
+                .id(churchMember.getId())
+                .name(churchMember.getName())
+                .householderId(churchMember.getHouseholderId())
+                .relationshipWithHouseholder(churchMember.getRelationshipWithHouseHolder())
+                .birthYear(churchMember.getBirthYear())
+                .salvationYear(churchMember.getSalvationYear())
+                .salvationMonth(churchMember.getSalvationMonth())
+                .salvationDay(churchMember.getSalvationDay())
+                .carNumber(churchMember.getCarNumber())
+                .sex(churchMember.getGender().getValue())
+                .phoneNumber(churchMember.getPhoneNumber())
+                .address(churchMember.getAddress())
+                .profileImageThumbnail(churchMember.getProfileImageThumbnail())
+                .churchId(churchMember.getChurch().getId())
+                .churchName(churchMember.getChurch().getName())
+                .cellId(churchMember.getCell().getId())
+                .cell(churchMember.getCell().getCell())
+                .gatheringId(churchMember.getGathering() != null? churchMember.getGathering().getId() : null)
+                .gatheringName(churchMember.getGathering() != null? churchMember.getGathering().getName() : null)
+                .positionId(churchMember.getPosition() != null ? churchMember.getPosition().getId() : null)
+                .positionName(churchMember.getPosition() != null ? churchMember.getPosition().getName() : null)
+                .ministryId(churchMember.getChurchMemberMinistryRoleAssociations().get(0).getId())
+                .ministryName(churchMember.getChurchMemberMinistryRoleAssociations().get(0).getMinistryRole().getMinistry().getName())
+                .ministryRole(churchMember.getChurchMemberMinistryRoleAssociations().get(0).getMinistryRole().getRole())
+                .build();
+    }
+
+    public MinistryLeadersResponse toMinistryLeadersResponse(List<MinistryLeaderDTO> ministryLeaderDTOS) {
+        return MinistryLeadersResponse.builder()
+                .churchMembers(ministryLeaderDTOS)
                 .build();
     }
 }
--- src/main/java/com/mohim/api/service/MinistryService.java
+++ src/main/java/com/mohim/api/service/MinistryService.java
@@ -2,10 +2,14 @@ package com.mohim.api.service;
 
 import com.mohim.api.dto.MinistriesResponse;
 import com.mohim.api.dto.MinistryDTO;
+import com.mohim.api.dto.MinistryLeaderDTO;
+import com.mohim.api.dto.MinistryLeadersResponse;
 import com.mohim.api.mapper.MinistryMapper;
+import com.mohim.api.repository.MemberRepository;
 import com.mohim.api.repository.MinistryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +20,7 @@ public class MinistryService {
 
     private final MinistryRepository ministryRepository;
     private final MinistryMapper ministryMapper;
+    private final MemberRepository memberRepository;
 
     public MinistriesResponse getMinistryList(Long churchId) {
         List<MinistryDTO> ministryDTOS = ministryRepository.findByChurchId(churchId).stream()
@@ -25,4 +30,12 @@ public class MinistryService {
         return ministryMapper.toMinistryResponse(ministryDTOS);
     }
 
+    @Transactional(readOnly = true)
+    public MinistryLeadersResponse getMinistryLeaders(Long churchId, Long ministryId) {
+        List<MinistryLeaderDTO> ministryLeaderDTOS = memberRepository.findChurchMembersByMinistryIdAndChurchId(ministryId, churchId).stream()
+                .map(churchMember -> {
+                    return ministryMapper.toMinistryLeaderDTO(churchMember);
+                }).collect(Collectors.toList());
+        return ministryMapper.toMinistryLeadersResponse(ministryLeaderDTOS);
+    }
 }
