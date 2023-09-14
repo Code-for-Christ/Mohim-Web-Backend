6cbe5e65e48c67e1abd158b1167a1d1975d41269
SungKyum Kim <sungkyum1@naver.com>
2023-09-15 00:19:43 +0900

    Feat: 회별 임원 목록 조회 API 구현을 위한 DTO 추가

---
+++ src/main/java/com/mohim/api/dto/MinistryLeaderDTO.java
@@ -0,0 +1,33 @@
+package com.mohim.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class MinistryLeaderDTO {
+    private Long id;
+    private String name;
+    private Long householderId;
+    private String relationshipWithHouseholder;
+    private Integer birthYear;
+    private Integer salvationYear;
+    private Integer salvationMonth;
+    private Integer salvationDay;
+    private String carNumber;
+    private String sex;
+    private String phoneNumber;
+    private String address;
+    private byte[] profileImageThumbnail;
+    private Long churchId;
+    private String churchName;
+    private Long cellId;
+    private Integer cell;
+    private Long gatheringId;
+    private String gatheringName;
+    private Long positionId;
+    private String positionName;
+    private Long ministryId;
+    private String ministryName;
+    private String ministryRole;
+}
