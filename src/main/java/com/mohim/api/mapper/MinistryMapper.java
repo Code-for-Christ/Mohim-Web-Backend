750818f075329396ec40ebf27108fa119735781a
SungKyum Kim <sungkyum1@naver.com>
2023-09-14 23:38:12 +0900

    Feat: MinistryMapper 구현

--- src/main/java/com/mohim/api/mapper/MinistryMapper.java
+++ src/main/java/com/mohim/api/mapper/MinistryMapper.java
@@ -1,2 +1,27 @@
-package com.mohim.api.mapper;public class MinistryMapper {
+package com.mohim.api.mapper;
+
+import com.mohim.api.domain.Ministry;
+import com.mohim.api.dto.MinistriesResponse;
+import com.mohim.api.dto.MinistryDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class MinistryMapper {
+
+    public MinistryDTO toMinistryDTO(Ministry ministry){
+        return MinistryDTO.builder()
+                .id(ministry.getId())
+                .name(ministry.getName())
+                .category(ministry.getCategory())
+                .churchId(ministry.getChurch().getId())
+                .build();
+    }
+
+    public MinistriesResponse toMinistryResponse(List<MinistryDTO> ministryDTOS) {
+        return MinistriesResponse.builder()
+                .ministries(ministryDTOS)
+                .build();
+    }
 }
