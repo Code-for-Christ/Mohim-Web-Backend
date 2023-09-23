b192730c2467012ec4e789e60b180e677851d453
SungKyum Kim <sungkyum1@naver.com>
2023-09-24 00:59:10 +0900

    Test: 변경된 DB 스펙에 맞추어 권한 테스트 수정

--- src/test/java/com/mohim/api/service/CustomUserDetailsServiceTest.java
+++ src/test/java/com/mohim/api/service/CustomUserDetailsServiceTest.java
@@ -28,7 +28,7 @@ class CustomUserDetailsServiceTest {
         UserDetails user = customUserDetailsService.loadUserByUsername("sungkyum1@gmail.com");
         // then
         assertThat(user.getAuthorities()).extracting(GrantedAuthority::getAuthority)
-                .contains("admin", "write", "read");
+                .contains("ADMIN", "write", "read");
         System.out.println("user's Authorities : " + user.getAuthorities());
     }
 }
