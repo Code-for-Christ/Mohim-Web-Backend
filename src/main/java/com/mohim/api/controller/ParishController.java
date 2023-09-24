c73b9f253e016bbcb9d14bc67f503e8424f5028a
SungKyum Kim <sungkyum1@naver.com>
2023-09-24 21:07:01 +0900

    Feat: get parish list API 구현

--- src/main/java/com/mohim/api/controller/ParishController.java
+++ src/main/java/com/mohim/api/controller/ParishController.java
@@ -1,2 +1,26 @@
-package com.mohim.api.controller;public class ParishController {
+package com.mohim.api.controller;
+
+import com.mohim.api.dto.ParishesResponse;
+import com.mohim.api.service.ParishService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/churches")
+@RequiredArgsConstructor
+public class ParishController {
+
+    private final ParishService parishService;
+
+    @GetMapping("/{church_id}/parishes")
+    public ResponseEntity<ParishesResponse> getParishList(@PathVariable("church_id") Long churchId) {
+        ParishesResponse response = parishService.getParishList(churchId);
+        return ResponseEntity.ok().body(response);
+    }
 }
--- src/main/java/com/mohim/api/service/ParishService.java
+++ src/main/java/com/mohim/api/service/ParishService.java
@@ -1,2 +1,25 @@
-package com.mohim.api.service;public class ParishService {
+package com.mohim.api.service;
+
+import com.mohim.api.dto.ParishesResponse;
+import com.mohim.api.repository.ParishRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ParishService {
+
+    private final ParishRepository parishRepository;
+
+    public ParishesResponse getParishList(Long churchId) {
+        List<Integer> parishes = parishRepository.findByChurchId(churchId).stream()
+                .map(parish -> parish.getId().intValue())
+                .collect(Collectors.toList());
+        return ParishesResponse.from(parishes);
+    }
 }
