47a66aa738ff184873b8f7a60db9faac0f549d9d
SungKyum Kim <sungkyum1@naver.com>
2023-09-24 21:07:24 +0900

    Test: get parish list Test 추가

--- src/test/java/com/mohim/api/service/ParishServiceTest.java
+++ src/test/java/com/mohim/api/service/ParishServiceTest.java
@@ -1,4 +1,28 @@
+package com.mohim.api.service;
+
+import com.mohim.api.dto.ParishesResponse;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class ParishServiceTest {
-  
+
+    @Autowired
+    ParishService parishService;
+
+    @Test
+    void getParishList() {
+        // given
+        Long churchId = 1L;
+
+        // when
+        ParishesResponse parishesResponse = parishService.getParishList(churchId);
+
+        // then
+        Assertions.assertThat(parishesResponse.getParishes().size()).isEqualTo(4);
+    }
 }
