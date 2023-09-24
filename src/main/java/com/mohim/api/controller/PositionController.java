5b6b41410e792c7e7d1284e360ef19f51f1c3499
SungKyum Kim <sungkyum1@naver.com>
2023-09-25 02:09:56 +0900

    Feat: get position list API 구현

--- src/main/java/com/mohim/api/controller/PositionController.java
+++ src/main/java/com/mohim/api/controller/PositionController.java
@@ -1,2 +1,26 @@
-package com.mohim.api.controller;public class PositionController {
+package com.mohim.api.controller;
+
+import com.mohim.api.dto.PositionsResponse;
+import com.mohim.api.service.PositionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/churches")
+@RequiredArgsConstructor
+public class PositionController {
+
+    private final PositionService positionService;
+
+    @GetMapping("/{church_id}/positions")
+    public ResponseEntity<PositionsResponse> getPositionList(@PathVariable("church_id") Long churchId) {
+        PositionsResponse response = positionService.getPositionList(churchId);
+        return ResponseEntity.ok().body(response);
+    }
 }
--- src/main/java/com/mohim/api/service/PositionService.java
+++ src/main/java/com/mohim/api/service/PositionService.java
@@ -1,2 +1,25 @@
-package com.mohim.api.service;public class PositionService {
+package com.mohim.api.service;
+
+import com.mohim.api.dto.PositionDTO;
+import com.mohim.api.dto.PositionsResponse;
+import com.mohim.api.repository.PositionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PositionService {
+
+    private final PositionRepository positionRepository;
+    public PositionsResponse getPositionList(Long churchId) {
+        List<PositionDTO> positionDTOS = positionRepository.findByChurchId(churchId).stream()
+                .map(position -> PositionDTO.from(position))
+                .collect(Collectors.toList());
+        return PositionsResponse.from(positionDTOS);
+    }
 }
