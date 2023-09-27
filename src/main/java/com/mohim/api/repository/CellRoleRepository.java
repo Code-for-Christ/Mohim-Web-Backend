d1c65bbcd5223e03b41b841299c747a2b1faf0fa
SungKyum Kim <sungkyum1@naver.com>
2023-09-28 01:04:46 +0900

    feat: add get cellroles API

--- src/main/java/com/mohim/api/repository/CellRoleRepository.java
+++ src/main/java/com/mohim/api/repository/CellRoleRepository.java
@@ -1,2 +1,15 @@
-package com.mohim.api.repository;public class CellRoleRepository {
+package com.mohim.api.repository;
+
+import com.mohim.api.domain.CellRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CellRoleRepository extends JpaRepository<CellRole, Long> {
+    @Query("SELECT cr FROM CellRole cr " +
+            "JOIN cr.cell c " +
+            "JOIN c.church ch " +
+            "WHERE ch.id = :churchId")
+    List<CellRole> findByChurchId(Long churchId);
 }
