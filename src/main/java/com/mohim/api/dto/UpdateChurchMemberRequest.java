4bc740276ce87d2fa2fa929a190b2dee85a41dc2
SungKyum Kim <sungkyum1@naver.com>
2023-09-30 00:38:28 +0900

    feat: add DTOs for update Church Member

---
+++ src/main/java/com/mohim/api/dto/UpdateChurchMemberRequest.java
@@ -0,0 +1,52 @@
+package com.mohim.api.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UpdateChurchMemberRequest {
+    @NotNull
+    private String name;
+    @NotNull
+    private String sex;
+    private String phoneNumber;
+    private String carNumber;
+    private Integer birthYear;
+    private Integer salvationYear;
+    private Integer salvationMonth;
+    private Integer salvationDay;
+    private MultipartFile profileImage;
+    @NotNull
+    private Long householderId;
+    @NotNull
+    private String relationshipWithHouseholder;
+    private String address;
+    @NotNull
+    private Boolean updateHouseholdAddress;
+    @NotNull
+    private Long parish;
+    private Long parishRoleId;
+    @NotNull
+    private Long cellId;
+    private Long cellRoleId;
+    @NotNull
+    private Long gatheringId;
+    private Long gatheringRoleId;
+    private Long positionId;
+    private List<MinistryDTO> ministries;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class MinistryDTO {
+        public Long ministryId;
+        public Long ministryRoleId;
+    }
+}
