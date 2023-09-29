e9e48e944c5fdf23275f037bdcbac5394a960d4f
SungKyum Kim <sungkyum1@naver.com>
2023-09-30 00:41:26 +0900

    feat: add file upload method

--- src/main/java/com/mohim/api/service/ProfileImageService.java
+++ src/main/java/com/mohim/api/service/ProfileImageService.java
@@ -1,16 +1,30 @@
 package com.mohim.api.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.mohim.api.domain.ChurchMember;
 import com.mohim.api.dto.ProfileImageUrlResponse;
 import com.mohim.api.exception.CustomException;
 import com.mohim.api.exception.ErrorCode;
 import com.mohim.api.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+import java.util.UUID;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProfileImageService {
@@ -36,4 +50,70 @@ public class ProfileImageService {
                 .profileImageUrl(imageUrl)
                 .build();
     }
+
+    public void uploadProfileImage(ChurchMember churchMember, MultipartFile multipartFile, String fileType) throws IOException {
+        // 1. 기존 프로필 이미지 정보 조회
+        String profileImageToBeDeleted = churchMember.getProfileImageName();
+
+        // 2. 썸네일 만들기
+        String thumbnail = createThumbnail(multipartFile, fileType);
+
+        // 3. 새 이미지 s3 업로드
+        // 3-1. 파일명 중복을 방지하기 위한 UUID 추가
+        String profileImageName = UUID.randomUUID().toString();
+
+        // 3-2. 메타데이터 설정
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(multipartFile.getContentType());
+        objectMetadata.setContentLength(multipartFile.getSize());
+
+        // 3-3. 업로드
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            String key = churchMember.getChurch().getId() + profileImageName + "." + fileType;
+            amazonS3.putObject(new PutObjectRequest(bucket, key, inputStream, objectMetadata)); // 이미지 업로드 확인용 퍼블릭 이미지 액세스 허용 옵션 추가
+        } catch (IOException e) {
+            log.error("S3 파일 업로드에 실패했습니다. {}", e.getMessage());
+            throw new CustomException(ErrorCode.UPLOAD_FAILED); // 파일 업로드 실패 에러를 던짐}
+        }
+
+        // 4. 새로운 프로필 이미지 DB 반영
+        churchMember.updateProfileImageName(profileImageName);
+        // 5. 썸네일 이미지 DB 반영
+        churchMember.updateProfileImageThumbnail(thumbnail);
+
+        // 6. 기존 프로필 이미지 삭제
+        if (profileImageToBeDeleted != null) {
+            String key = churchMember.getChurch().getId() + profileImageToBeDeleted;
+            amazonS3.deleteObject(bucket, key);
+        }
+    }
+
+    private String createThumbnail(MultipartFile file, String fileType) throws IOException {
+        try {
+            // MultipartFile에서 이미지 데이터 읽기
+            BufferedImage originalImage = ImageIO.read(file.getInputStream());
+
+            // 이미지를 250x250 크기로 조정
+            int thumbnailWidth = 250;
+            int thumbnailHeight = 250;
+            BufferedImage thumbnail = new BufferedImage(thumbnailWidth, thumbnailHeight, BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics2D = thumbnail.createGraphics();
+            graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            graphics2D.drawImage(originalImage, 0, 0, thumbnailWidth, thumbnailHeight, null);
+            graphics2D.dispose();
+
+            // 조정된 이미지를 Base64로 인코딩
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(thumbnail, fileType, baos);
+            byte[] imageBytes = baos.toByteArray();
+            String base64Thumbnail = Base64.getEncoder().encodeToString(imageBytes);
+
+            return base64Thumbnail;
+        } catch (IOException e) {
+            // 오류 처리
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
 }
