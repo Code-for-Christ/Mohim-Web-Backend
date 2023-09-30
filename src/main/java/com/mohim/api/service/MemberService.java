5fd667687041ab11c881d5654a00e14a212a4908
SungKyum Kim <sungkyum1@naver.com>
2023-10-01 02:15:25 +0900

    fix: Verification and algorithm modification

--- src/main/java/com/mohim/api/service/MemberService.java
+++ src/main/java/com/mohim/api/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.mohim.api.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.mohim.api.constant.Gender;
 import com.mohim.api.domain.*;
 import com.mohim.api.dto.*;
@@ -44,7 +47,6 @@ public class MemberService {
     private final ProfileImageService profileImageService;
 
     private final String[] ALLOWED_EXTENSIONS = {"jpg", "jpeg", "png"};
-
     public ChurchMembersResponse getChurchMembers(Integer churchId, ChurchMembersRequest request, HttpServletRequest httpServletRequest) {
         List<ChurchMembersResponse.ChurchMember> churchMembers = memberRepository.findByChurchId(churchId, request).stream()
                 .map(churchMember -> {
@@ -104,8 +106,8 @@ public class MemberService {
                 .build();
     }
 
-    public ChurchMemberResponse getChurchMember(Integer churchId, Integer memberId) {
-        ChurchMember churchMember = memberRepository.findByIdAndChurchId(memberId.longValue(), churchId.longValue()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+    public ChurchMemberResponse getChurchMember(Long churchId, Long memberId) {
+        ChurchMember churchMember = memberRepository.findByIdAndChurchId(memberId, churchId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
 
         List<Ministry> ministries = churchMemberMinistryAssociationRepository.findAllByChurchMember(churchMember).stream()
                 .map(ChurchMemberMinistryAssociation::getMinistry)
@@ -132,7 +134,7 @@ public class MemberService {
             }
         }
 
-        List<ChurchMember> householdMembers = memberRepository.findAllByHouseholderId(churchMember.getHouseholderId());
+        List<ChurchMember> householdMembers = memberRepository.findAllByChurchIdAndHouseholderId(churchId, churchMember.getHouseholderId());
 
         List<ChurchMemberResponse.Household> householdResponses = new ArrayList<>();
         for (ChurchMember householdMember : householdMembers) {
@@ -188,80 +190,130 @@ public class MemberService {
     }
 
     @Transactional
-    public void updateChurchMember(Long churchId, Long memberId, UpdateChurchMemberRequest request) throws IOException {
+    public UpdateChurchMemberResponse updateChurchMember(Long churchId, Long memberId, UpdateChurchMemberRequest request) throws IOException {
 
         // 멤버 검증 및 가져오기
         ChurchMember churchMember = memberRepository.findByIdAndChurchId(memberId, churchId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
-        ChurchMember houseHoldMember = memberRepository.findByIdAndChurchId(churchMember.getHouseholderId(), churchId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
 
-        String[] fileNames = request.getProfileImage().getOriginalFilename().split("\\.");
-        String fileName = fileNames[fileNames.length - 1];
+        // 교구 검증 및 가져오기
+        Parish upatedParish = parishRepository.findByChurchIdAndId(churchId, request.getParish()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_PARISH));
+        // 구역 검증 및 가져오기
+        Cell updatedCell = cellRepository.findByChurchIdAndId(churchId, request.getCellId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_CELL));
 
-        for (String extension : ALLOWED_EXTENSIONS) {
-            if (extension.equalsIgnoreCase(fileName)){
-                throw new CustomException(ErrorCode.INVALID_EXTENSION);
-            }
-        }
-
-        profileImageService.uploadProfileImage(churchMember, request.getProfileImage(), fileName);
-
+        // TODO finished
         // 교구에 맞는 구역 검증
-        if (request.getCellId() / request.getParish() != 10) {
+        if (!updatedCell.getParish().getId().equals(upatedParish.getId())) {
             throw new CustomException(ErrorCode.INVALID_PARISH_OR_CELL);
         }
 
-        // 교구 검증 및 가져오기
-        Parish upatedParish = parishRepository.findById(request.getParish()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_PARISH));
-        // 구역 검증 및 가져오기
-        Cell updatedCell = cellRepository.findById(request.getCellId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_CELL));
         // 회(모임) 검증 및 가져오기
-        Gathering updatedGathering = gatheringRepository.findById(request.getGatheringId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_GATHERING));
-        // 전도인 역할 가져오기
-        Position updatedPosition = positionRepository.findById(request.getPositionId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_POSITION));
+        Gathering updatedGathering = gatheringRepository.findByChurchIdAndId(churchId, request.getGatheringId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_GATHERING));
+        // 직분 가져오기
+        Position updatedPosition = positionRepository.findByChurchIdAndId(churchId, request.getPositionId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_POSITION));
 
         // 교구 역할 검증 및 추가
-        ParishRole parishRole = parishRoleRepository.findById(request.getParishRoleId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_PARISH_ROLE));
-        ChurchMemberParishRoleAssociation churchMemberParishRoleAssociation = ChurchMemberParishRoleAssociation.createChurchMemberParishRoleAssociation(churchMember, parishRole);
-        churchMemberParishRoleAssociationRepository.save(churchMemberParishRoleAssociation);
+        // TODO churchId 추가 finished
+        // TODO 가지고 있는 역할이 있는지 확인 먼저 해야함 있으면 수정 없다면 추가
+        // 요청한 parishRole 이 있는지 검증
+        ParishRole parishRole = parishRoleRepository.findByChurchIdAndId(churchId, request.getParishRoleId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_PARISH_ROLE));
+        // 요청한 멤버의 기존 parishRole 이 있는지 조회
+        ChurchMemberParishRoleAssociation churchMemberParishRoleAssociation = churchMemberParishRoleAssociationRepository.findByChurchMember(churchMember);
+
+        if (churchMemberParishRoleAssociation == null) { // 없다면 새로 생성
+            ChurchMemberParishRoleAssociation createdAssociation = ChurchMemberParishRoleAssociation.createChurchMemberParishRoleAssociation(churchMember, parishRole);
+            churchMemberParishRoleAssociationRepository.save(createdAssociation);
+        } else { // 있다면 수정
+            churchMemberParishRoleAssociation.updateParishRole(parishRole);
+            churchMemberParishRoleAssociationRepository.save(churchMemberParishRoleAssociation);
+        }
 
         // 구역 역할 검증 및 추가
-        CellRole cellRole = cellRoleRepository.findById(request.getCellRoleId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_CELL_ROLE));
-        ChurchMemberCellRoleAssociation churchMemberCellRoleAssociation = ChurchMemberCellRoleAssociation.createChurchMemberCellRoleAssociation(churchMember, cellRole);
-        churchMemberCellRoleAssociationRepository.save(churchMemberCellRoleAssociation);
+        // TODO churchId 추가 finished
+        // TODO 가지고 있는 역할이 있는지 확인 먼저 해야함 있으면 수정 없다면 추가
+        // 요청한 cellRole 존재 검증
+        CellRole cellRole = cellRoleRepository.findByChurchIdAndId(churchId, request.getCellRoleId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_CELL_ROLE));
+        // 요청한 멤버의 기존 cellRole 여부 조회
+        ChurchMemberCellRoleAssociation churchMemberCellRoleAssociation = churchMemberCellRoleAssociationRepository.findByChurchMember(churchMember);
+        if (churchMemberCellRoleAssociation == null) {
+            ChurchMemberCellRoleAssociation createdAssociation = ChurchMemberCellRoleAssociation.createChurchMemberCellRoleAssociation(churchMember, cellRole);
+            churchMemberCellRoleAssociationRepository.save(createdAssociation);
+        } else {
+            churchMemberCellRoleAssociation.updateCellRole(cellRole);
+            churchMemberCellRoleAssociationRepository.save(churchMemberCellRoleAssociation);
+        }
 
         // 회별 역할 검증 및 추가
-        GatheringRole gatheringRole = gatheringRoleRepository.findById(request.getGatheringRoleId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_GATHERING_ROLE));
-        ChurchMemberGatheringRoleAssociation churchMemberGatheringRoleAssociation = ChurchMemberGatheringRoleAssociation.createChurchMemberGatheringRoleAssociation(churchMember, gatheringRole);
-        churchMemberGatheringRoleAssociationRepository.save(churchMemberGatheringRoleAssociation);
+        // TODO churchId 추가
+        // TODO 가지고 있는 역할이 있는지 확인 먼저 해야함 있으면 수정 없다면 추가
+        // gatheringRole 검증
+        GatheringRole gatheringRole = gatheringRoleRepository.findByChurchIdAndId(churchId, request.getGatheringRoleId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_GATHERING_ROLE));
+        // 요청 멤버의 기존 회별 역할 여부 조회
+        ChurchMemberGatheringRoleAssociation churchMemberGatheringRoleAssociation = churchMemberGatheringRoleAssociationRepository.findByChurchMember(churchMember);
+        if (churchMemberGatheringRoleAssociation == null) {
+            ChurchMemberGatheringRoleAssociation createdAssociation = ChurchMemberGatheringRoleAssociation.createChurchMemberGatheringRoleAssociation(churchMember, gatheringRole);
+            churchMemberGatheringRoleAssociationRepository.save(createdAssociation);
+        } else {
+            churchMemberGatheringRoleAssociation.updateGatheringRole(gatheringRole);
+            churchMemberGatheringRoleAssociationRepository.save(churchMemberGatheringRoleAssociation);
+        }
 
         // 봉사 및 봉사 역할 검증 및 추가
-        for (UpdateChurchMemberRequest.MinistryDTO ministryDTO: request.getMinistries()) {
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new SimpleModule());
+        List<UpdateChurchMemberRequest.MinistryDTO> ministries = objectMapper.readValue(request.getMinistries(), new TypeReference<List<UpdateChurchMemberRequest.MinistryDTO>>() {
+        });
+
+        List<ChurchMemberMinistryAssociation> associations = new ArrayList<>();
+        List<ChurchMemberMinistryRoleAssociation> roleAssociations = new ArrayList<>();
+
+        for (UpdateChurchMemberRequest.MinistryDTO ministryDTO: ministries) {
             // 봉사
-            Ministry ministry = ministryRepository.findById(ministryDTO.ministryId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MINISTRY));
+            // TODO 먼저 내 ID로 가지고 있는 association 목록을 모두 지우고 받은 정보들을 한번에 save
+            Ministry ministry = ministryRepository.findByChurchIdAndId(churchId, ministryDTO.ministryId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MINISTRY));
             ChurchMemberMinistryAssociation churchMemberMinistryAssociation = ChurchMemberMinistryAssociation.createChurchMemberMinistryAssociation(churchMember, ministry);
-            churchMemberMinistryAssociationRepository.save(churchMemberMinistryAssociation);
+            associations.add(churchMemberMinistryAssociation);
 
             // 봉사 역할
             if (ministryDTO.ministryRoleId != null) {
-                MinistryRole ministryRole = ministryRoleRepository.findById(ministryDTO.ministryRoleId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MINISTRY_ROLE));
+                MinistryRole ministryRole = ministryRoleRepository.findByChurchIdAndId(churchId, ministryDTO.ministryRoleId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MINISTRY_ROLE));
                 ChurchMemberMinistryRoleAssociation churchMemberMinistryRoleAssociation = ChurchMemberMinistryRoleAssociation.createChurchMemberMinistryRoleAssociation(churchMember, ministryRole);
-                churchMemberMinistryRoleAssociationRepository.save(churchMemberMinistryRoleAssociation);
+                roleAssociations.add(churchMemberMinistryRoleAssociation);
+            }
+        }
+        // 문제 없이 추가 완료시 기존 것 삭제
+        churchMemberMinistryAssociationRepository.deleteAllByChurchMemberId(memberId);
+        churchMemberMinistryRoleAssociationRepository.deleteAllByChurchMemberId(memberId);
+
+        // 벌크 save
+        churchMemberMinistryAssociationRepository.saveAll(associations);
+        churchMemberMinistryRoleAssociationRepository.saveAll(roleAssociations);
+
+        // 세대주 주소 변경 여부 확인 및 변경
+        // TODO householdId가 있다면 새로 업데이트, 없다면 자기로 getUpdateHouseholdAddress 여부에 따라 세대원 주소 바꾸는 것도 고려
+        // TODO
+        Long updatedHouseholderId = churchMember.getHouseholderId();
+        if (request.getHouseholderId() != null) {
+            updatedHouseholderId = request.getHouseholderId();
+        }
+
+        List<ChurchMember> churchMembers = new ArrayList<>();
+        if (request.getUpdateHouseholdAddress()) {
+            List<ChurchMember> householdMembers = memberRepository.findAllByChurchIdAndHouseholderId(churchId, updatedHouseholderId);
+            for (ChurchMember householdMember : householdMembers) {
+                if (!householdMember.getId().equals(memberId)) {
+                    householdMember.updateAddress(request.getAddress());
+                    churchMembers.add(householdMember);
+                }
             }
         }
 
-        // 세대주 주소 변경 여부 확인 및 변경
-        if (request.getUpdateHouseholdAddress()) {
-            houseHoldMember.updateAddress(request.getAddress());
-            memberRepository.save(houseHoldMember);
-        }
-
+        // TODO 성도 추가 시에는 플러쉬 해서 없는 ID 미리 받아오는 것 고려 / flush 안하고도 householdId와 자기 id를 맞춰줄수 있는 방법 있는지 확인
         // 정보 업데이트
         churchMember.updateChurchMember(
                 request.getName(),
                 updatedCell,
                 updatedGathering,
                 updatedPosition,
-                request.getHouseholderId(),
+                updatedHouseholderId,
                 request.getRelationshipWithHouseholder(),
                 request.getBirthYear(),
                 request.getSalvationYear(),
@@ -272,7 +324,32 @@ public class MemberService {
                 request.getPhoneNumber(),
                 request.getAddress()
                 );
+        churchMembers.add(churchMember);
 
-        memberRepository.save(churchMember);
+        // 파일 타입 검증
+        if (!request.getProfileImage().isEmpty()) {
+            String[] fileNames = request.getProfileImage().getOriginalFilename().split("\\.");
+            String fileType = fileNames[fileNames.length - 1];
+
+            boolean isAllowedExtension = false;
+
+            for (String extension : ALLOWED_EXTENSIONS) {
+                if (fileType.equalsIgnoreCase(extension)){
+                    isAllowedExtension = true;
+                    break;
+                }
+            }
+            if (!isAllowedExtension) {
+                throw new CustomException(ErrorCode.INVALID_EXTENSION);
+            }
+
+            // 이미지 업로드
+            profileImageService.uploadProfileImage(churchMember, request.getProfileImage(), fileType);
+        }
+
+        // 커밋
+        memberRepository.saveAll(churchMembers);
+
+        return UpdateChurchMemberResponse.from(memberId);
     }
 }
