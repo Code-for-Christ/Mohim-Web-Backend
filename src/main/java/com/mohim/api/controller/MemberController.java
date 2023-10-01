2a238bb744e1b4e1f94d3bbf0b68a62441fd710b
SungKyum Kim <sungkyum1@naver.com>
2023-10-01 22:19:40 +0900

    feat: Added initBinder to inject null when multipart File is requested as an empty string

--- src/main/java/com/mohim/api/controller/MemberController.java
+++ src/main/java/com/mohim/api/controller/MemberController.java
@@ -9,10 +9,13 @@ import com.mohim.api.service.ProfileImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
 import java.io.IOException;
 import java.util.List;
 
@@ -48,9 +51,24 @@ public class MemberController {
 
     // TODO churchMember id return finished
     @PutMapping("/{church_id}/church-members/{church_member_id}")
-    public ResponseEntity<UpdateChurchMemberResponse> updateChurchMember(@PathVariable("church_id") Long churchId, @PathVariable("church_member_id") Long memberId, @Valid UpdateChurchMemberRequest request) throws IOException {
+    public ResponseEntity<UpdateChurchMemberResponse> updateChurchMember(@PathVariable("church_id") Long churchId, @PathVariable("church_member_id") Long memberId, @ModelAttribute @Valid UpdateChurchMemberRequest request) throws IOException {
         UpdateChurchMemberResponse response = memberService.updateChurchMember(churchId, memberId, request);
         return ResponseEntity.ok().body(response);
     }
+
+    // multipart File 이 빈 스트링으로 요청되어질 때 null을 주입해주는 initBinder 추가
+    @InitBinder
+    public void initBinder(WebDataBinder binder) throws Exception {
+        binder.registerCustomEditor(MultipartFile.class, new PropertyEditorSupport() {
+
+            @Override
+            public void setAsText(String text) {
+                log.debug("initBinder MultipartFile.class: {}; set null;", text);
+                setValue(null);
+            }
+
+        });
+    }
+
 
 }
